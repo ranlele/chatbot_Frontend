ddc026afe22d1bfccfb51d8678a8fa7db3b05ce6
Ranlele <jr5816@nyu.edu>
2023-05-01 15:12:44 -0400

    Improve UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [chatRecords, setChatRecords] = useState([]);
+  const [userQuestion, setUserQuestion] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    fetchChatRecords();
+  }, []);
+
+  async function fetchChatRecords() {
+    try {
+      const response = await axios.get('http://localhost:8080/chat-records');
+      setChatRecords(response.data);
+    } catch (error) {
+      console.error('Error fetching chat records:', error);
+    }
+  }
+
+  async function handleAskQuestion(event) {
+    event.preventDefault();
+    if (!userQuestion.trim()) {
+      return;
+    }
+  
+    setIsLoading(true);
+    try {
+      const response = await axios.post('http://localhost:8080/api/ask-question', {
+        question: userQuestion,
+      });
+  
+      const newChatRecord = response.data;
+      setChatRecords([...chatRecords, newChatRecord]);
+      setUserQuestion('');
+    } catch (error) {
+      console.error('Error asking question:', error);
+    }
+    setIsLoading(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Chat Records</h1>
+      <div className="chat-container">
+        {chatRecords.map((chatRecord, index) => (
+          <div key={index}>
+            <p className="user-question">You: {chatRecord.question}</p>
+            <p className="ai-answer">AI: {chatRecord.answer}</p>
+          </div>
+        ))}
+      </div>
+      <form className="question-form" onSubmit={handleAskQuestion}>
+        <input
+          className="question-input"
+          type="text"
+          value={userQuestion}
+          onChange={(event) => setUserQuestion(event.target.value)}
+          placeholder="Ask a question"
+        />
+        <button className="submit-button" type="submit">
+          Submit
+        </button>
+      </form>
+      {isLoading && <p>Loading...</p>}
     </div>
   );
+  
 }
 
 export default App;
